8842437dc40d5844f36b057910c1c6a767d4464b
silvio.ribeiro <silviojgfr@gmail.com.ribeiro>
2020-09-06 01:55:01 -0300

    ajuste do cors

--- src/http-common.js
+++ src/http-common.js
@@ -5,8 +5,5 @@ export default axios.create({
   baseURL: 'https://grades-api-sr.herokuapp.com',
   headers: {
     'Content-type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH',
-    'Access-Control-Allow-Credentials': true,
   },
 });
